ab6517a51da79f231cc6c8fad9a06a35c0653616
Kh4n <asadullahmkhan@gmail.com>
2019-10-11 22:55:43 -0500

    added multiprocessing, various speed improvements ran some tests as well

--- basic_skeleton.py
+++ basic_skeleton.py
@@ -4,7 +4,13 @@ import numpy as np
 import pandas as pd
 import cust_datagen
 
-CSV_COMBINED = <ENTER PATH TO combined.out HERE>
+# CHANGE THESE PATHS
+CSV_COMBINED = "/mnt/CSE-CIC-2018/Processed Traffic Data for ML Algorithms/combined_train"
+CSV_VALIDATION = "/mnt/CSE-CIC-2018/Processed Traffic Data for ML Algorithms/combined_val"
+
+# links to combined train/val data:
+# https://drive.google.com/file/d/11yVYZgVJE2zgGkPPzuSOO06MqTNCRDVV/view?usp=sharing
+# https://drive.google.com/file/d/1nEVzJdtVZUbNzBmgLRCm6b5E7yOoM0Kl/view?usp=sharing
 
 inputs = keras.Input(shape=(76), name='in')
 x = layers.Dense(64, activation='relu')(inputs)
@@ -13,11 +19,17 @@ outputs = layers.Dense(2, activation='softmax')(x)
 
 model = keras.Model(inputs=inputs, outputs=outputs, name='ids_model')
 
-gen = cust_datagen.IDSDataGenerator({"Benign": 0, "Malicious": 1}, 8000000, CSV_COMBINED, batch_size=256)
+# I have set these very high to test. If you get memory errors, reduce them
+steps_per_epoch = 1000
+batch_size = 1024
+
+gen = cust_datagen.IDSDataGenerator({"Benign": 0, "Malicious": 1}, CSV_COMBINED, (76), steps_per_epoch, batch_size=batch_size)
+vgen = cust_datagen.IDSDataGenerator({"Benign": 0, "Malicious": 1}, CSV_VALIDATION, (76), steps_per_epoch, batch_size=batch_size)
 
 model.compile(loss='mse',
               optimizer=keras.optimizers.RMSprop(),
               metrics=['accuracy'])
 
-history = model.fit_generator(gen,
-                    epochs=5, steps_per_epoch=10)
+history = model.fit_generator(
+    gen, epochs=5, validation_data=vgen, workers=12
+)
--- cust_datagen.py
+++ cust_datagen.py
@@ -2,38 +2,32 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import pandas as pd
+import psutil
 
 class IDSDataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, classes, num_rows, combined_csv, batch_size=32, shuffle=False):
+    def __init__(self, classes, combined_csv, input_dims, steps_per_epoch, batch_size=32):
         'Initialization'
         self.batch_size = batch_size
         self.classes = classes
-        self.shuffle = shuffle
-        self.num_rows = num_rows
         self.combined_csv = combined_csv
-        self.on_epoch_end()
+        self.dims = input_dims
+
+        self.steps_per_epoch = steps_per_epoch
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(self.num_rows / self.batch_size))
+        return self.steps_per_epoch
 
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
         # indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-        length = len(self)
 
         df = pd.read_csv(self.combined_csv, sep=',', skiprows=index*self.batch_size, nrows=self.batch_size)
         x = df.iloc[:,3:-1].values
-        y = [0 if k=="Benign" else 1 for k in df.iloc[:,-1:].values.tolist()]
+        y = [0 if k==["Benign"] else 1 for k in df.iloc[:,-1:].values.tolist()]
         y = keras.utils.to_categorical(y, num_classes=len(self.classes))
-        # print(x, y)
-        return x.astype(np.float), y
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        # self.indexes = np.arange(len(self.list_IDs))
-        # if self.shuffle == True:
-        #     np.random.shuffle(self.indexes)
+        return x.astype(np.float), y
         
