7aa9341c1e02a04c3f3c3a94f107cf9c4073b65f
Kh4n <asadullahmkhan@gmail.com>
2019-10-14 21:59:06 -0500

    made lots of changes, redownload the data files as they have been normalized now Aj A

--- basic_skeleton.py
+++ basic_skeleton.py
@@ -2,20 +2,32 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import cust_datagen
 
+def cust_accuracy(y_true, y_pred):
+    diff = tf.abs(y_true - y_pred)
+    correct = tf.math.less(diff, 0.5)
+    correct = tf.cast(correct, tf.float32)
+    return tf.math.reduce_mean(correct)
+
 # CHANGE THESE PATHS
-CSV_COMBINED = "/mnt/CSE-CIC-2018/Processed Traffic Data for ML Algorithms/combined_train"
+CSV_TRAIN = "/mnt/CSE-CIC-2018/Processed Traffic Data for ML Algorithms/combined_train"
 CSV_VALIDATION = "/mnt/CSE-CIC-2018/Processed Traffic Data for ML Algorithms/combined_val"
 
-# links to combined train/val data:
+# links to combined train/val data (IF YOU GET ANY ERRORS REDOWNLOAD THE DATA):
 # https://drive.google.com/file/d/11yVYZgVJE2zgGkPPzuSOO06MqTNCRDVV/view?usp=sharing
-# https://drive.google.com/file/d/1nEVzJdtVZUbNzBmgLRCm6b5E7yOoM0Kl/view?usp=sharing
+# https://drive.google.com/file/d/1ZjtGgooqZ0qRd_10MSy7Ds93aS3Z4v7p/view?usp=sharing
 
-inputs = keras.Input(shape=(76), name='in')
-x = layers.Dense(64, activation='relu')(inputs)
+inputs = layers.Input(shape=(76), name='in')
+x = layers.Dense(64, activation='relu', use_bias=False)(inputs)
+x = layers.BatchNormalization()(x)
+x = layers.Dense(64, activation='relu', use_bias=False)(x)
+x = layers.BatchNormalization()(x)
 x = layers.Dense(64, activation='relu')(x)
+x = layers.Dropout(0.4)(x)
 outputs = layers.Dense(2, activation='softmax')(x)
+
 
 model = keras.Model(inputs=inputs, outputs=outputs, name='ids_model')
 
@@ -23,10 +35,10 @@ model = keras.Model(inputs=inputs, outputs=outputs, name='ids_model')
 steps_per_epoch = 1000
 batch_size = 1024
 
-gen = cust_datagen.IDSDataGenerator({"Benign": 0, "Malicious": 1}, CSV_COMBINED, (76), steps_per_epoch, batch_size=batch_size)
-vgen = cust_datagen.IDSDataGenerator({"Benign": 0, "Malicious": 1}, CSV_VALIDATION, (76), steps_per_epoch, batch_size=batch_size)
+gen = cust_datagen.IDSDataGeneratorBasic({"Benign": 0, "Malicious": 1}, CSV_TRAIN, (76), steps_per_epoch, batch_size=batch_size)
+vgen = cust_datagen.IDSDataGeneratorBasic({"Benign": 0, "Malicious": 1}, CSV_VALIDATION, (76), steps_per_epoch, batch_size=batch_size)
 
-model.compile(loss='mse',
+model.compile(loss='binary_crossentropy',
               optimizer=keras.optimizers.RMSprop(),
               metrics=['accuracy'])
 
--- cust_datagen.py
+++ cust_datagen.py
@@ -4,7 +4,97 @@ import numpy as np
 import pandas as pd
 import psutil
 
-class IDSDataGenerator(keras.utils.Sequence):
+norm_cols = {
+    'Dst Port': [0, 65535],
+    'Protocol': [0, 17],
+    'Flow Duration': [-828220000000, 120000000],
+    'Tot Fwd Pkts': [1, 58091],
+    'Tot Bwd Pkts': [0, 123118],
+    'TotLen Fwd Pkts': [0, 9789747],
+    'TotLen Bwd Pkts': [0.0, 156360426],
+    'Fwd Pkt Len Max': [0, 64440],
+    'Fwd Pkt Len Min': [0, 1460],
+    'Fwd Pkt Len Mean': [0.0, 16529.3138401559],
+    'Fwd Pkt Len Std': [0.0, 18401.5827717299],
+    'Bwd Pkt Len Max': [0, 65160],
+    'Bwd Pkt Len Min': [0, 1460],
+    'Bwd Pkt Len Mean': [0.0, 33879.28358],
+    'Bwd Pkt Len Std': [0.0, 21326.2385],
+    'Flow Byts/s': [0.0, 19005438.5894467],
+    'Flow Pkts/s': [-0.0088953248, 2000000.0],
+    'Flow IAT Mean': [-828220000000.0, 120000000.0],
+    'Flow IAT Std': [0.0, 474354474600.909],
+    'Flow IAT Max': [-828220000000, 968434000000],
+    'Flow IAT Min': [-947405000000, 120000000.0],
+    'Fwd IAT Tot': [-828220000000, 120000000.0],
+    'Fwd IAT Mean': [-828220000000.0, 120000000.0],
+    'Fwd IAT Std': [0.0, 474354474600.909],
+    'Fwd IAT Max': [-828220000000, 968434000000],
+    'Fwd IAT Min': [-947405000000, 120000000.0],
+    'Bwd IAT Tot': [0.0, 120000000.0],
+    'Bwd IAT Mean': [0.0, 120000000.0],
+    'Bwd IAT Std': [0.0, 84800000.0],
+    'Bwd IAT Max': [0.0, 120000000.0],
+    'Bwd IAT Min': [0.0, 120000000.0],
+    'Fwd PSH Flags': [0, 1],
+    'Bwd PSH Flags': [0, 0],
+    'Fwd URG Flags': [0, 1],
+    'Bwd URG Flags': [0, 0],
+    'Fwd Header Len': [0, 2275036],
+    'Bwd Header Len': [0, 2462372],
+    'Fwd Pkts/s': [0.0, 4000000.0],
+    'Bwd Pkts/s': [0.0, 2000000.0],
+    'Pkt Len Min': [0, 1460],
+    'Pkt Len Max': [0, 65160],
+    'Pkt Len Mean': [0.0, 17344.98473],
+    'Pkt Len Std': [0.0, 22788.28621],
+    'Pkt Len Var': [0.0, 519000000.0],
+    'FIN Flag Cnt': [0, 1],
+    'SYN Flag Cnt': [0, 1],
+    'RST Flag Cnt': [0, 1],
+    'PSH Flag Cnt': [0, 1],
+    'ACK Flag Cnt': [0, 1],
+    'URG Flag Cnt': [0, 1],
+    'CWE Flag Count': [0, 1],
+    'ECE Flag Cnt': [0, 1],
+    'Down/Up Ratio': [0, 237],
+    'Pkt Size Avg': [0.0, 17478.40769],
+    'Fwd Seg Size Avg': [0.0, 16529.3138401559],
+    'Bwd Seg Size Avg': [0.0, 33879.28358],
+    'Fwd Byts/b Avg': [0, 0],
+    'Fwd Pkts/b Avg': [0, 0],
+    'Fwd Blk Rate Avg': [0, 0],
+    'Bwd Byts/b Avg': [0, 0],
+    'Bwd Pkts/b Avg': [0, 0],
+    'Bwd Blk Rate Avg': [0, 0],
+    'Subflow Fwd Pkts': [1, 58091],
+    'Subflow Fwd Byts': [0, 9789747],
+    'Subflow Bwd Pkts': [0, 123118],
+    'Subflow Bwd Byts': [0, 156360426],
+    'Init Fwd Win Byts': [-1, 65535],
+    'Init Bwd Win Byts': [-1, 65535],
+    'Fwd Act Data Pkts': [0, 18290],
+    'Fwd Seg Size Min': [0, 56],
+    'Active Mean': [0.0, 114000000.0],
+    'Active Std': [0.0, 74900000.0],
+    'Active Max': [0.0, 114000000],
+    'Active Min': [0.0, 114000000],
+    'Idle Mean': [0.0, 395571421052.63104],
+    'Idle Std': [0.0, 262247866338.599],
+    'Idle Max': [0.0, 968434000000],
+    'Idle Min': [0.0, 239934000000]
+}
+
+def handle_nan_inf(s):
+    if s == "Infinity":
+        return float("inf")
+    else:
+        return float(s)
+
+converters = {"Flow Byts/s": handle_nan_inf, "Flow Pkts/s": handle_nan_inf}
+
+
+class IDSDataGeneratorBasic(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, classes, combined_csv, input_dims, steps_per_epoch, batch_size=32):
         'Initialization'
@@ -24,10 +114,44 @@ class IDSDataGenerator(keras.utils.Sequence):
         # Generate indexes of the batch
         # indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
-        df = pd.read_csv(self.combined_csv, sep=',', skiprows=index*self.batch_size, nrows=self.batch_size)
+        df = pd.read_csv(self.combined_csv, sep=',', skiprows=range(1,index*self.batch_size), nrows=self.batch_size, converters=converters)
+        for c in norm_cols:
+            df[c] = (df[c] - norm_cols[c][0])/(norm_cols[c][1]-norm_cols[c][0])
+
         x = df.iloc[:,3:-1].values
-        y = [0 if k==["Benign"] else 1 for k in df.iloc[:,-1:].values.tolist()]
+        y = df["Label"].apply(lambda s: 0 if s=="Benign" else 1)
         y = keras.utils.to_categorical(y, num_classes=len(self.classes))
+
+        return x.astype(np.float), y
+
+class IDSDataGeneratorAttention(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, classes, combined_csv, input_dims, steps_per_epoch, batch_size=32):
+        'Initialization'
+        self.batch_size = batch_size
+        self.classes = classes
+        self.combined_csv = combined_csv
+        self.dims = input_dims
+
+        self.steps_per_epoch = steps_per_epoch
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return self.steps_per_epoch
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        # indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        group_size = self.batch_size*self.dims[0]
+        df = pd.read_csv(self.combined_csv, sep=',', skiprows=range(1,index*group_size), nrows=group_size, converters=converters)
+        for c in norm_cols:
+            df[c] = (df[c] - norm_cols[c][0])/(norm_cols[c][1]-norm_cols[c][0])
+
+        x = np.reshape(df.iloc[:,3:-1].values, [self.batch_size, *self.dims]) 
+        y = [0 if k==["Benign"] else 1 for k in df.iloc["Label"].values.tolist()[self.dims[0]-1::self.dims[0]]]
+        y = np.reshape(keras.utils.to_categorical(y, num_classes=len(self.classes)), [self.batch_size, self.dims[0]*len(self.classes)])
 
         return x.astype(np.float), y
         
