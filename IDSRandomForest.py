83564370651dde38dd848d27777828020e289cdc
VishalMadipadga <38477510+VishalMadipadga@users.noreply.github.com>
2019-11-09 17:55:58 -0600

    Add files via upload

--- IDSRandomForest.py
+++ IDSRandomForest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+import h5py
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -11,14 +12,15 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
+
 def main():
-    benign = 'Benign'
-    #df = pd.read_csv('Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv',dtype={'Flow Byts/s':float,'Flow Pkts/s':float})
-    df = pd.read_csv('Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv')
-    #s = df[df.columns[20]]
-    #t = df[df.columns[21]]
-    
-    df['Flow Byts/s'] = df['Flow Byts/s'].convert_objects(convert_numeric=True)
+    H5_COMBINED = 'combined.hdf5'
+    combined_h5 = h5py.File(H5_COMBINED, 'r') 
+    data = combined_h5["combined"][:]
+    df = pd.read_csv('Friday-23-02-2018_TrafficForML_CICFlowMeter.csv')
+    del df['Label']
+
+    '''df['Flow Byts/s'] = df['Flow Byts/s'].convert_objects(convert_numeric=True)
     df['Flow Pkts/s'] = df['Flow Pkts/s'].convert_objects(convert_numeric=True)
     df['Label'].replace(benign , 0,inplace=True)
     df.loc[df.Label != 0, 'Label'] = 1
@@ -31,24 +33,32 @@ def main():
     labels = df['Label']
     df.replace('', np.nan, inplace = True)
     df.dropna(inplace =True)
-    del df['Label']
-    X_train, X_test, y_train, y_test = train_test_split(df, labels, test_size = 0.2, random_state = 0)
+    del df['Label']'''
+    labels = data[:,-1]
+     
+    data = np.delete(data, -1, 1)
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2, random_state = 0)
     #X_train.fillna(X_train.mean(), inplace=True)
     #X_train.dropna(inplace=True)
     DTreeClf = DecisionTreeClassifier(criterion = 'entropy')
     DTreeClf.fit(X_train, y_train)
     decisionTree = DTreeClf.tree_
     features = DTreeClf.feature_importances_
-    y_pred = clf.predict(X_test)
+    y_pred = DTreeClf.predict(X_test)
     confusionMatrix = confusion_matrix(y_test, y_pred)
-    print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
-    print("Precision: ", metrics.precision_score(y_test, y_pred))
-    print("Recall: ", metrics.recall_score(y_test, y_pred))
-    print("F1: ", metrics.f1_score(y_test, y_pred))
-    print('Features: ')
-    print(features)   
+    print("Accuracy: ", accuracy_score(y_test, y_pred))
+    print("Precision: ", precision_score(y_test, y_pred))
+    print("Recall: ", recall_score(y_test, y_pred))
+    print("F1: ", f1_score(y_test, y_pred))   
     print('Confusion Matrix: ')
     print(confusionMatrix)
+
+    featureDict = dict()
+    for i in range(len(df.columns)):
+        featureDict.update({df.columns[i]:features[i]})
+    print('\033[1m'+'Features of Importance:'+'\033[0m')
+    #print(featureDict)
+    print(sorted(featureDict.items(), key=lambda x: x[1], reverse=True))
 
 
 
